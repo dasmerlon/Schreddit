b521b69f27cc8f16a8105916020ac31a2b684a4e
Erik Macbook Pro <alerik8@yahoo.de>
2021-06-08 22:52:17 +0200

    Added Like-Buttons, Header-Info of the Post & Tag

--- frontend/schreddit-ui/src/components/Post.js
+++ frontend/schreddit-ui/src/components/Post.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {makeStyles} from "@material-ui/core/styles";
-import {Card, CardHeader, Avatar, SvgIcon} from "@material-ui/core";
+import {Card, CardHeader, Avatar, SvgIcon, Link, Grid, CardActionArea, Box, Chip} from "@material-ui/core";
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
@@ -14,13 +14,14 @@ import BookmarkBorderRoundedIcon from '@material-ui/icons/BookmarkBorderRounded'
 import ShareRoundedIcon from '@material-ui/icons/ShareRounded';
 import MoreHorizRoundedIcon from '@material-ui/icons/MoreHorizRounded';
 // Source: https://materialdesignicons.com/
-import { mdiGiftOutline, mdiCommentOutline } from '@mdi/js';
+import { mdiGiftOutline, mdiCommentOutline, mdiArrowUpBoldOutline, mdiArrowDownBoldOutline} from '@mdi/js';
 
 const useStyles = makeStyles({
     root: {
       //minWidth: 300,
       //maxWidth: 650,
       display: "flex",
+      
     },
     media: {
       height: 0,
@@ -49,7 +50,6 @@ const useStyles = makeStyles({
 //        mit Hintergrund verwenden.
 export default function Posts() {
     const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
   
     return (
         <Card useStyles={classes.root}>
@@ -60,19 +60,47 @@ export default function Posts() {
               </Avatar>
             }
             action={
-              <Button size="small">+Join</Button>
+              <Grid container>
+                <Button size="small">+Join</Button>
+
+                <Grid item direction="vertical">
+                  <IconButton size="small" title="More" onClick={()=>{alert('Upvote') }}> 
+                    <SvgIcon ><path d={mdiArrowUpBoldOutline} /></SvgIcon>
+                  </IconButton>
+                  <Typography component="h2">
+                    200
+                  </Typography> 
+                  <IconButton size="small" title="More" onClick={()=>{alert('Downvote') }}> 
+                    <SvgIcon ><path d={mdiArrowDownBoldOutline} /></SvgIcon>
+                  </IconButton>
+                </Grid>
+              </Grid>
             }
             title={
-              <Typography varaint="body1">r/phtography</Typography>,
-              <Typography variant="body1">gepostet von Erik</Typography>
+              <Link href="http://localhost:3000/" color="inherit">
+                {'r/HowToPictures'}
+              </Link>
+            }
+            subheader={
+              <Typography component="h2">
+                Posted by 
+                <Link href="http://localhost:3000/" color="inherit">
+                  {' u/Pictureman180 '}
+                </Link> 
+                2 days ago
+              </Typography>
             }
           />
+
+          <CardActionArea href="http://localhost:3000/">
           <CardContent>
             <Typography variant="h5" component="h2">
-              How does Shutter, ISO & Apature effect your pictures? (Title + Chip/Tag) 
+              How does Shutter, ISO & Apature effect your pictures? 
+              <Chip label="Informative 👨‍🎓" color="secondary" href="http://localhost:3000/" clickable />
             </Typography>
           </CardContent>
           <CardMedia className={classes.media} image={im} title="b"/>
+          </CardActionArea>
           <CardActions>
             <Button size="small" title="Comments" startIcon={<SvgIcon ><path d={mdiCommentOutline} /></SvgIcon>} onClick={()=>{alert('Go to comments') }} >Comments</Button>
             <Button size="small" title="Give Award" startIcon={<SvgIcon ><path d={mdiGiftOutline} /></SvgIcon>} onClick={()=>{alert('Give Award') }}>Award</Button>
