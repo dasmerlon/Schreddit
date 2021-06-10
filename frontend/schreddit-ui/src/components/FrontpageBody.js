c23bd6da452ad35f6773598940a2c7532648b2da
Erik Macbook Pro <alerik8@yahoo.de>
2021-06-10 14:40:05 +0200

    Added Sticky function to Info.js & created PopC.js

--- frontend/schreddit-ui/src/components/FrontpageBody.js
+++ frontend/schreddit-ui/src/components/FrontpageBody.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {makeStyles} from "@material-ui/core/styles";
-import {Grid, Paper, Container, Hidden} from "@material-ui/core";
+import {Grid, Paper, Container, Hidden, CssBaseline} from "@material-ui/core";
 import Post from "./Post";
 import SortByBar from "./SortByBar";
 import CreatePost from "./CreatePost";
 import TrendingComs from "./TrendingComs";
+import CommunitiesByCategory from "./PopularComs";
 import Info from "./Info";
 
 const useStyles = makeStyles((theme) => ({
@@ -13,7 +14,7 @@ const useStyles = makeStyles((theme) => ({
     },
     grid: {
       width: '100%',
-      margin: '0px'
+      margin: '0px',
     },
     paper: {
       padding: theme.spacing(20),
@@ -23,12 +24,17 @@ const useStyles = makeStyles((theme) => ({
     }
   }));
 
+//TODO: - Sticky funktionalität vom Contact Component ist hardcoded über die höhe der Seite..
+//      - Back to top Button hinzufügen
 export default function ForntpageBody() {
     const classes = useStyles();
 
     return (
     <div className={classes.root} > 
-      <Container >
+    <React.Fragment>
+      <CssBaseline />
+
+      <Container fixed >
         <Grid container spacing={2} direction='row' className={classes.grid}>
           <Grid item container spacing={3} direction='column' className={classes.grid} xs={12} md={7}>
             <Grid item>
@@ -46,6 +52,12 @@ export default function ForntpageBody() {
             <Grid item>
             <Post />
             </Grid>
+            <Grid item>
+            <Post />
+            </Grid>
+            <Grid item>
+            <Post />
+            </Grid>
           </Grid>
           <Grid item container spacing={3} direction='column' className={classes.grid} xs={1}>
             <Hidden smDown>
@@ -53,21 +65,23 @@ export default function ForntpageBody() {
                 <TrendingComs />
               </Grid>
               <Grid item>
+                <CommunitiesByCategory />
+              </Grid>
+              <Grid item>
+                <Paper className={classes.paper}>Top Communities (work in Progress)</Paper>
+              </Grid>
+              <Grid item>
+                <Paper className={classes.paper}>Premium (work in Progress)</Paper>
+              </Grid>
+              <Grid item>
                 <Info />
-              </Grid>
-              <Grid item>
-                <Paper className={classes.paper}>Trending (work in Progress)</Paper>
-              </Grid>
-              <Grid item>
-                <Paper className={classes.paper}>Home (work in Progress)</Paper>
-              </Grid>
-              <Grid item>
-                <Paper className={classes.paper}>Contact (work in Progress)</Paper>
               </Grid>
             </Hidden>
           </Grid>
         </Grid>
       </Container>
+
+    </React.Fragment>
     </div>
     );
 } 
--- frontend/schreddit-ui/src/components/Info.js
+++ frontend/schreddit-ui/src/components/Info.js
@@ -1,77 +1,195 @@
 import React from 'react';
 import {makeStyles} from "@material-ui/core/styles";
-import {Card, List, ListItem, ListItemSecondaryAction, ListItemText, ListSubheader, Link} from "@material-ui/core"
+import {Card, List, ListItem, ListItemSecondaryAction, ListItemText, ListSubheader, Link, CardContent, Typography} from "@material-ui/core"
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        //display: "flex",
-    },
+        position: "sticky",
+        top: "1rem",
+        hight: "3000px",
+    }
+    
   }));
 
+// TODO: Find a better way to make the Info-Card sticky
 export default function Info() {
     const classes = useStyles();
     
     return (
+        <>
         <Card className={classes.root}>
-            <List subheader={<ListSubheader>Trending Communities</ListSubheader>} className={classes.root}>
-                <ListItem>
-                    <ListItemText
-                        primary={
-                            <Link href="#"  color="inherit">
-                                {'Help'}
-                            </Link>
-                        }
-                    />
-                    <ListItemSecondaryAction>
+            <CardContent>
+                <List dense>
+                    <ListItem>
                         <ListItemText
                             primary={
                                 <Link href="#"  color="inherit">
-                                    {'About'}
+                                    {'Help'}
                                 </Link>
                             }
                         />
-                    </ListItemSecondaryAction>
-                </ListItem>
-                
-                <ListItem>
-                    <ListItemText
-                        primary={
-                            <Link href="#"  color="inherit">
-                                {'Reddit App'}
-                            </Link>
-                        }
-                    />
-                    <ListItemSecondaryAction>
+                        <ListItemSecondaryAction>
+                            <ListItemText
+                                primary={
+                                    <Link href="#"  color="inherit">
+                                        {'About'}
+                                    </Link>
+                                }
+                            />
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                    <ListItem>
                         <ListItemText
                             primary={
                                 <Link href="#"  color="inherit">
-                                    {'Careers'}
+                                    {'Reddit App'}
                                 </Link>
                             }
                         />
-                    </ListItemSecondaryAction>
-                </ListItem>
-
-                <ListItem>
-                    <ListItemText
-                        primary={
-                            <Link href="#"  color="inherit">
-                                {'Reddit Coins'}
-                            </Link>
-                        }
-                    />
-                    <ListItemSecondaryAction>
+                        <ListItemSecondaryAction>
+                            <ListItemText
+                                primary={
+                                    <Link href="#"  color="inherit">
+                                        {'Careers'}
+                                    </Link>
+                                }
+                            />
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                    <ListItem>
                         <ListItemText
                             primary={
                                 <Link href="#"  color="inherit">
-                                    {'Press'}
+                                    {'Reddit Coins'}
                                 </Link>
                             }
                         />
-                    </ListItemSecondaryAction>
-                </ListItem>
-            </List>
+                        <ListItemSecondaryAction>
+                            <ListItemText
+                                primary={
+                                    <Link href="#"  color="inherit">
+                                        {'Press'}
+                                    </Link>
+                                }
+                            />
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText
+                            primary={
+                                <Link href="#"  color="inherit">
+                                    {'Reddit Premium'}
+                                </Link>
+                            }
+                        />
+                        <ListItemSecondaryAction>
+                            <ListItemText
+                                primary={
+                                    <Link href="#"  color="inherit">
+                                        {'Advertise'}
+                                    </Link>
+                                }
+                            />
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText
+                            primary={
+                                <Link href="#"  color="inherit">
+                                    {'Reddit Gifts'}
+                                </Link>
+                            }
+                        />
+                        <ListItemSecondaryAction>
+                            <ListItemText
+                                primary={
+                                    <Link href="#"  color="inherit">
+                                        {'Blog'}
+                                    </Link>
+                                }
+                            />
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText
+                            primary={
+                                <Link href="#"  color="inherit">
+                                    {'Communities'}
+                                </Link>
+                            }
+                        />
+                        <ListItemSecondaryAction>
+                            <ListItemText
+                                primary={
+                                    <Link href="#"  color="inherit">
+                                        {'Terms'}
+                                    </Link>
+                                }
+                            />
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText
+                            primary={
+                                <Link href="#"  color="inherit">
+                                    {'Rereddit'}
+                                </Link>
+                            }
+                        />
+                        <ListItemSecondaryAction>
+                            <ListItemText
+                                primary={
+                                    <Link href="#"  color="inherit">
+                                        {'Content Policy'}
+                                    </Link>
+                                }
+                            />
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText
+                            primary={
+                                <Link href="#"  color="inherit">
+                                    {'Topics'}
+                                </Link>
+                            }
+                        />
+                        <ListItemSecondaryAction>
+                            <ListItemText
+                                primary={
+                                    <Link href="#"  color="inherit">
+                                        {'Privacy Policy'}
+                                    </Link>
+                                }
+                            />
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText/>
+                        <ListItemSecondaryAction>
+                            <ListItemText
+                                primary={
+                                    <Link href="#"  color="inherit">
+                                        {'Mod Policy'}
+                                    </Link>
+                                }
+                            />
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                </List>
+                <Typography>
+                    <br />
+                    Reddit Inc © 2021 . All rights reserved
+                </Typography>
+            </CardContent>
         </Card>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        </>
     );
   }
 
