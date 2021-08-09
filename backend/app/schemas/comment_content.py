a683eb42c87341bf24edcfa6bfab3abbae3fddcc
Jascha Kühn <jascha.kuehn@studium.uni-hamburg.de>
2021-08-09 18:11:44 +0200

    prevent text to be set to None

--- backend/app/schemas/comment_content.py
+++ backend/app/schemas/comment_content.py
@@ -1,14 +1,21 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, constr, validator
 
 
 class CommentContentBase(BaseModel):
     text: Optional[constr(min_length=1, strip_whitespace=True)] = None
 
+    @validator("text")
+    def text_must_not_be_none(cls, v):
+        if v is None:
+            raise ValueError("text must not be None")
+        else:
+            return v
+
 
 class CommentContentCreate(CommentContentBase):
-    text: str
+    text: constr(min_length=1, strip_whitespace=True)
 
 
 class CommentContentUpdate(CommentContentBase):
