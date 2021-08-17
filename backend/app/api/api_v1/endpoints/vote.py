c472be706ea333b1be87edab0f4ca3b0b8cf03b7
Jascha Kühn <jascha.kuehn@studium.uni-hamburg.de>
2021-08-17 14:05:37 +0200

    add vote endpoints & tests

---
+++ backend/app/api/api_v1/endpoints/vote.py
@@ -0,0 +1,82 @@
+from fastapi import APIRouter, Depends, Response, status
+from pydantic import UUID4
+
+from app import crud, models, schemas
+from app.api import deps
+from app.api.api_v1.exceptions import ThingAlreadyVoted, ThingNotFoundException
+
+router = APIRouter()
+
+
+@router.put(
+    "/{uid}/{dir}",
+    name="Vote on a thing",
+    response_class=Response,
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+def vote(
+    uid: UUID4,
+    dir: schemas.VoteOptions,
+    current_user: models.User = Depends(deps.get_current_user),
+):
+    """
+    Vote a post up or down or remove a vote.
+
+    - `uid` : UUID of the post
+    - `dir` : direction of the vote,
+    `1` for upvotes, `-1` for downvotes and `0` for unvoting
+    """
+    thing_meta = crud.thing_meta.get(uid)
+    if not thing_meta:
+        raise ThingNotFoundException
+
+    state = crud.thing_meta.get_vote_state(thing_meta, current_user)
+
+    if state == dir:
+        raise ThingAlreadyVoted
+
+    if dir == schemas.VoteOptions.upvote:
+        crud.thing_meta.upvote(thing_meta, current_user, state)
+    elif dir == schemas.VoteOptions.downvote:
+        crud.thing_meta.downvote(thing_meta, current_user, state)
+    elif dir == schemas.VoteOptions.novote:
+        crud.thing_meta.remove_vote(thing_meta, current_user, state)
+
+
+@router.get(
+    "/{uid}/state",
+    name="Get vote state on a thing",
+    response_model=schemas.VoteOptions,
+    status_code=status.HTTP_200_OK,
+)
+def get_vote_state(
+    uid: UUID4, current_user: models.User = Depends(deps.get_current_user)
+):
+    """
+    Get current vote state of a thing.
+
+    :param uid: UUID of the post
+    :return current vote -- `1` for upvoted, `-1` for downvoted and `0` for unvoted
+    """
+    thing_meta = crud.thing_meta.get(uid)
+    if not thing_meta:
+        raise ThingNotFoundException
+
+    return crud.thing_meta.get_vote_state(thing_meta, current_user)
+
+
+@router.get(
+    "/{uid}/count", name="Get vote count on a thing", status_code=status.HTTP_200_OK
+)
+def get_vote_count(uid: UUID4):
+    """
+    Get current vote count of a thing.
+
+    :param uid: UUID of the post
+    :return current vote count
+    """
+    thing_meta = crud.thing_meta.get(uid)
+    if not thing_meta:
+        raise ThingNotFoundException
+
+    return crud.thing_meta.get_vote_count(thing_meta)
---
+++ backend/app/tests/api/api_v1/test_votes.py
@@ -0,0 +1,170 @@
+from uuid import uuid4
+
+import pytest
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from app.core.config import settings
+from app.models import CommentContent, CommentMeta, PostContent, PostMeta, User
+
+
+@pytest.mark.parametrize("dir", [1, -1])
+@pytest.mark.parametrize("type", ["post", "comment"])
+def test_vote(
+    client: TestClient,
+    fake_auth: User,
+    comment_in_db_other_user: (CommentMeta, CommentContent),
+    post_self_in_db_other_user: (PostMeta, PostContent),
+    dir: int,
+    type: str,
+) -> None:
+    if type == "post":
+        metadata = post_self_in_db_other_user[0]
+    elif type == "comment":
+        metadata = comment_in_db_other_user[0]
+    r = client.put(f"{settings.API_V1_STR}/vote/{metadata.uid}/{dir}")
+    assert r.status_code == status.HTTP_204_NO_CONTENT
+
+    r = client.get(f"{settings.API_V1_STR}/vote/{metadata.uid}/state")
+    assert r.status_code == status.HTTP_200_OK
+    state = r.json()
+    assert state == dir
+
+    r = client.get(f"{settings.API_V1_STR}/vote/{metadata.uid}/count")
+    assert r.status_code == status.HTTP_200_OK
+    count = r.json()
+    assert count == dir
+
+
+@pytest.mark.parametrize("type", ["post", "comment"])
+def test_upvote_already_upvoted(
+    client: TestClient,
+    fake_auth: User,
+    comment_in_db_upvoted: (CommentMeta, CommentContent),
+    post_self_in_db_upvoted: (PostMeta, PostContent),
+    type: str,
+) -> None:
+    if type == "post":
+        metadata = post_self_in_db_upvoted[0]
+    elif type == "comment":
+        metadata = comment_in_db_upvoted[0]
+    r = client.put(f"{settings.API_V1_STR}/vote/{metadata.uid}/1")
+    assert r.status_code == status.HTTP_304_NOT_MODIFIED
+    assert "detail" in r.json()
+
+    r = client.get(f"{settings.API_V1_STR}/vote/{metadata.uid}/state")
+    assert r.status_code == status.HTTP_200_OK
+    state = r.json()
+    assert state == 1
+
+    r = client.get(f"{settings.API_V1_STR}/vote/{metadata.uid}/count")
+    assert r.status_code == status.HTTP_200_OK
+    count = r.json()
+    assert count == 1
+
+
+@pytest.mark.parametrize("type", ["post", "comment"])
+def test_remove_vote(
+    client: TestClient,
+    fake_auth: User,
+    comment_in_db_upvoted: (CommentMeta, CommentContent),
+    post_self_in_db_upvoted: (PostMeta, PostContent),
+    type: str,
+) -> None:
+    if type == "post":
+        metadata = post_self_in_db_upvoted[0]
+    elif type == "comment":
+        metadata = comment_in_db_upvoted[0]
+    r = client.put(f"{settings.API_V1_STR}/vote/{metadata.uid}/0")
+    assert r.status_code == status.HTTP_204_NO_CONTENT
+
+    r = client.get(f"{settings.API_V1_STR}/vote/{metadata.uid}/state")
+    assert r.status_code == status.HTTP_200_OK
+    state = r.json()
+    assert state == 0
+
+    r = client.get(f"{settings.API_V1_STR}/vote/{metadata.uid}/count")
+    assert r.status_code == status.HTTP_200_OK
+    count = r.json()
+    assert count == 0
+
+
+@pytest.mark.parametrize("dir", [1, -1])
+@pytest.mark.parametrize("type", ["post", "comment"])
+def test_vote_nonexisting_thing(
+    client: TestClient,
+    fake_auth: User,
+    comment_in_db_other_user: (CommentMeta, CommentContent),
+    post_self_in_db_other_user: (PostMeta, PostContent),
+    dir: int,
+    type: str,
+) -> None:
+    if type == "post":
+        metadata = post_self_in_db_other_user[0]
+    elif type == "comment":
+        metadata = comment_in_db_other_user[0]
+    r = client.put(f"{settings.API_V1_STR}/vote/{uuid4()}/{dir}")
+    assert r.status_code == status.HTTP_404_NOT_FOUND
+    assert "detail" in r.json()
+
+    r = client.get(f"{settings.API_V1_STR}/vote/{metadata.uid}/state")
+    assert r.status_code == status.HTTP_200_OK
+    state = r.json()
+    assert state == 0
+
+    r = client.get(f"{settings.API_V1_STR}/vote/{metadata.uid}/count")
+    assert r.status_code == status.HTTP_200_OK
+    count = r.json()
+    assert count == 0
+
+
+@pytest.mark.parametrize("type", ["post", "comment"])
+def test_get_vote_count_nonexisting_thing(
+    client: TestClient,
+    fake_auth: User,
+    comment_in_db_other_user: (CommentMeta, CommentContent),
+    post_self_in_db_other_user: (PostMeta, PostContent),
+    type: str,
+) -> None:
+    if type == "post":
+        metadata = post_self_in_db_other_user[0]
+    elif type == "comment":
+        metadata = comment_in_db_other_user[0]
+    r = client.get(f"{settings.API_V1_STR}/vote/{uuid4()}/count")
+    assert r.status_code == status.HTTP_404_NOT_FOUND
+
+    r = client.get(f"{settings.API_V1_STR}/vote/{metadata.uid}/state")
+    assert r.status_code == status.HTTP_200_OK
+    state = r.json()
+    assert state == 0
+
+    r = client.get(f"{settings.API_V1_STR}/vote/{metadata.uid}/count")
+    assert r.status_code == status.HTTP_200_OK
+    count = r.json()
+    assert count == 0
+
+
+@pytest.mark.parametrize("type", ["post", "comment"])
+def test_get_vote_state_nonexisting_thing(
+    client: TestClient,
+    fake_auth: User,
+    comment_in_db_other_user: (CommentMeta, CommentContent),
+    post_self_in_db_other_user: (PostMeta, PostContent),
+    type: str,
+) -> None:
+    if type == "post":
+        metadata = post_self_in_db_other_user[0]
+    elif type == "comment":
+        metadata = comment_in_db_other_user[0]
+    r = client.get(f"{settings.API_V1_STR}/vote/{uuid4()}/state")
+    assert r.status_code == status.HTTP_404_NOT_FOUND
+
+    r = client.get(f"{settings.API_V1_STR}/vote/{metadata.uid}/state")
+    assert r.status_code == status.HTTP_200_OK
+    state = r.json()
+    assert state == 0
+
+    r = client.get(f"{settings.API_V1_STR}/vote/{metadata.uid}/count")
+    assert r.status_code == status.HTTP_200_OK
+    count = r.json()
+    assert count == 0
