330ad06caa8b8703318b455322385f2eeea8111c
Pascal Kröger <pascalkroger@Pascals-MacBook-Pro.local>
2021-09-18 17:14:45 +0200

    added all implemented sorts to frontend

--- frontend/schreddit-ui/src/components/SortByBar.js
+++ frontend/schreddit-ui/src/components/SortByBar.js
@@ -14,11 +14,11 @@ export default function SortByBar(props) {
     return (
         <Card>
             <CardActions>
-                <Button size="medium" title="Best" startIcon={ <SvgIcon ><path d={mdiRocket} /></SvgIcon>}>Best</Button>
-                <Button size="medium" title="Hot" startIcon={<WhatshotSharpIcon />} onClick={()=>{alert('Show Hot') }}>Hot</Button>
+                <Button size="medium" title="Best" startIcon={ <SvgIcon ><path d={mdiRocket} onClick={()=>{props.getPosts('best')}} /></SvgIcon>}>Best</Button>
+                <Button size="medium" title="Hot" startIcon={<WhatshotSharpIcon />} onClick={()=>{props.getPosts('hot')}}>Hot</Button>
                 <Button size="medium" title="New" startIcon={ <SvgIcon ><path d={mdiAlertDecagram} /></SvgIcon>} onClick={()=>{props.getPosts('new') }}>New</Button>
                 <Button size="medium" title="Top" startIcon={ <SvgIcon ><path d={mdiFormatVerticalAlignTop} /></SvgIcon>} onClick={()=>{props.getPosts('top') }}>Top</Button>
-                <Button size="medium" title="Rising" startIcon={<TrendingUpRoundedIcon />} onClick={()=>{alert('Show Rising') }}>Rising</Button>
+                {/* <Button size="medium" title="Rising" startIcon={<TrendingUpRoundedIcon />} onClick={()=>{props.getPosts('top') }}>Rising</Button> */}
             </CardActions>
         </Card>
     );
