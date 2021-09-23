85620798c2fbf138b4f2e067a31fa837961400d6
Jascha Kühn <jascha.kuehn@studium.uni-hamburg.de>
2021-09-23 16:54:46 +0200

    add crud operation to get post tree

--- backend/app/crud/crud_post_meta.py
+++ backend/app/crud/crud_post_meta.py
@@ -4,7 +4,7 @@ from neomodel import db
 
 from app import models, schemas
 from app.crud.crud_thing_meta import CRUDThingBaseMeta
-from app.crud.cypher import CypherGetPosts
+from app.crud.cypher import CypherGetPosts, CypherGetTree
 
 
 class CRUDPostMeta(
@@ -64,6 +64,62 @@ class CRUDPostMeta(
             #  way to do this in the Cypher query yet
             post_list.reverse()
         return post_list
+
+    @db.read_transaction
+    def get_post_tree(self, post: models.PostMeta, user: Optional[models.User]):
+        """
+        Get the post tree for a post.
+
+        :param post: the post to get the tree for
+        :param user: if specified, the user to get vote states for;
+        if ``None``, don't get vote states
+        :return: a tree-like dict that contains the comments associated with the post
+        """
+        cgt = CypherGetTree(user)
+        query = cgt.get_tree_query()
+        params = {"post_uid": post.uid}
+        results, columns = db.cypher_query(query, params)
+        tree = results[0][0]
+
+        # collect uids of things
+        uids = []
+        stack = list(tree.items())
+        while stack:
+            k, v = stack.pop()
+            if k == "parent" and isinstance(v, list):
+                for comment in v:
+                    stack.extend(comment.items())
+            elif k == "uid":
+                uids.append(v)
+
+        # get vote state and vote count for uids
+        query = cgt.get_votes_query()
+        params = {"thing_uids": uids}
+        if user:
+            params["user_uid"] = user.uid
+        results, columns = db.cypher_query(query, params)
+
+        # iterate through tree and add authors and vote state/count
+        stack = [tree]
+        while stack:
+            thing = stack.pop()
+            thing_uid = thing["uid"]
+            # search result index
+            idx = [row[0] for row in results].index(thing_uid)
+            thing["author"] = results[idx][1]
+            thing["count"] = results[idx][2]
+            thing["state"] = results[idx][3]
+            children = thing.get("parent")
+            if children and isinstance(children, list):
+                for thing in children:
+                    stack.append(thing)
+            elif children is None:  # add empty children list if no children exist
+                thing["parent"] = []
+        return tree, uids
+
+    @db.read_transaction
+    def get_subreddit(self, db_obj: models.PostMeta):
+        return db_obj.subreddit.single()
 
     @db.write_transaction
     def set_subreddit(
