f58bac442f1a2c4a4e7156db1ea6977e252b3330
Saman Khosravi <8khosrav@informatik.uni-hamburg.de>
2021-09-24 14:39:41 +0200

    minor fixes + file extension

--- backend/app/api/api_v1/endpoints/blob.py
+++ backend/app/api/api_v1/endpoints/blob.py
@@ -1,3 +1,4 @@
+import os
 from uuid import uuid4
 
 from azure.storage.blob import BlobClient, ContentSettings
@@ -20,8 +21,10 @@ def upload(file: UploadFile = File(...)):
     Upload a video or image as a Blob to Azure Storage.
     Returns the URL of the saved file.
 
-    :param file: the file to be uploaded
+    - `file` : the file to be uploaded
     """
+    split = os.path.splitext(file)
+    file_extension = split[1]
     if file.content_type.startswith("image"):
         container = "images"
     elif file.content_type.startswith("video"):
@@ -29,7 +32,7 @@ def upload(file: UploadFile = File(...)):
     blob = BlobClient.from_connection_string(
         conn_str=settings.AZURE_STORAGE_CREDENTIALS,
         container_name=container,
-        blob_name=uuid4().hex,
+        blob_name=uuid4().hex + file_extension,
     )
     content_settings = ContentSettings(content_type=file.content_type)
     blob.upload_blob(file.file, content_settings=content_settings)
--- backend/app/api/api_v1/main.py
+++ backend/app/api/api_v1/main.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import (account, auth, comments, posts,
-                                      subreddits, subscription, users, vote, blob)
+from app.api.api_v1.endpoints import (account, auth, blob, comments, posts,
+                                      subreddits, subscription, users, vote)
 
 router = APIRouter()
 router.include_router(account.router, prefix="/account")
 router.include_router(auth.router, prefix="/auth")
+router.include_router(blob.router, prefix="/upload")
 router.include_router(comments.router, prefix="/comments")
 router.include_router(posts.router, prefix="/posts")
 router.include_router(subreddits.router, prefix="/subreddits")
 router.include_router(subscription.router, prefix="/subscription")
 router.include_router(users.router, prefix="/users")
 router.include_router(vote.router, prefix="/vote")
-router.include_router(blob.router, prefix="/upload")
