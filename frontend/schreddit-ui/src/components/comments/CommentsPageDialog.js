dc19936388f3d036587e27d407148bf89a49ca28
Erik Macbook Pro <alerik8@yahoo.de>
2021-09-25 21:08:55 +0200

    Go through comment response tree

--- frontend/schreddit-ui/src/components/comments/CommentsPageDialog.js
+++ frontend/schreddit-ui/src/components/comments/CommentsPageDialog.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {makeStyles} from "@material-ui/core/styles";
-import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, IconButton, SvgIcon, Typography} from "@material-ui/core";
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, Grid, IconButton, SvgIcon, Typography} from "@material-ui/core";
 import Post from "../Post";
 import Comment from "./Comment";
+import configData from '../config.json';
+import axios from 'axios';
 
 // Source: https://materialdesignicons.com/
-import { mdiArrowUpBoldOutline, mdiArrowDownBoldOutline} from '@mdi/js';
+import { mdiArrowUpBoldOutline, mdiArrowDownBoldOutline, mdiCommentMinus, mdiConsoleNetworkOutline} from '@mdi/js';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -24,6 +27,24 @@ const useStyles = makeStyles((theme) => ({
 
 export default function CommentsPageBody(props) {
     const classes = useStyles();
+
+    const [sortBy, setSortByValue] = React.useState("?sort=top"); 
+    const [comInfo, setComInfo] = React.useState("");
+    const [list, setList] = React.useState("");
+
+    useEffect(() => {
+        if(props.open) {
+            axios.get(configData.POST_API_URL + "4a2c8573-9d1e-49f2-8665-3fbefa32834e" + "/tree" + sortBy)
+            .then(userResponse => {
+                console.log("list ", list, "; userResponse ", userResponse)
+                setComInfo(commentMaker(userResponse.data, setList, list));
+                console.log("list", list)
+            })
+            .catch(error => {
+                setComInfo("Post could not load... Please try again later.");
+            })
+        } else {}
+    }, [props.open]);
 
     const commentInfos = [
         {
@@ -91,7 +112,7 @@ export default function CommentsPageBody(props) {
         },
     ];
 
-    const comInfo = commentInfos.map((entry) =>
+    const comInfo2 = commentInfos.map((entry) =>
         <Grid item>
             <Comment commenterName={entry["commenterName"]} commentText={entry["commentText"]} commentOn={entry["indent"]}/>
         </Grid>);
@@ -118,7 +139,7 @@ export default function CommentsPageBody(props) {
                 <DialogContentText id="scroll-dialog-description" tabIndex={-1}>
                     <Grid container spacing={3} direction='column' className={classes.grid} >
                         <Grid item>
-                            <Post showVotes={false} showJoin={false}/>
+                            <Post clickable={false} showVotes={false} showJoin={false}/>
                         </Grid>
                         {comInfo}
                         
@@ -132,3 +153,55 @@ export default function CommentsPageBody(props) {
         </Dialog>
     );
 } 
+
+function commentMaker(postTree, setList, list) {
+    setList();
+    var a = postTree.children;
+    console.log("commentmaker")
+
+    for (var i = 0; i < postTree.children.length; i++) {
+      var b = a[i];
+      whileLoop(b, setList, list);
+      console.log("back to mainLoop")
+    }
+
+    return 0;
+  }
+
+  function forLoop(b, setList, list) {
+    console.log("newFor", b)
+    setList(list=>[list, b.content.text]);
+
+    for (var j = 0; j < b.children.length; j++) {
+      console.log("j" + j + "; b-length" + b.children.length)
+      
+      console.log("for", b)
+      whileLoop(b.children[j], setList, list);
+      console.log("back from while")
+    }
+    return 0;
+  }
+
+  function whileLoop(b, setList, list) {
+    var bLength = b.children.length;
+
+    while (bLength > 0) {
+        console.log("while", b)
+      if (b.children.length === 1) {
+        setList(list=>[list, b.content.text]);
+        console.log(b.content.text)
+        console.log(list)
+        b = b.children[0];
+        bLength = b.children.length
+      } else if (b.children.length > 1) {
+        console.log("while elif")
+        forLoop(b, setList, list);
+        bLength = -1
+      }
+    }
+    if (b.children.length === 0) {
+        console.log("while 0children", b)
+        setList(list=>[list, b.content.text]);
+    }
+    return 0;
+  }
