822c2cdcec94a2fc293bf820e33911cb6513aff4
Jascha Kühn <jascha.kuehn@studium.uni-hamburg.de>
2021-09-27 09:30:38 +0000

    Merged PR 57: Enhance security of upload endpoint
        - only authorized users can upload
        - user is saved with blob as metadata
        - actual file content is analyzed before uploading
        - file extension is taken from analysis instead of the original extension
    
    Related work items: #131

--- backend/app/api/api_v1/endpoints/blob.py
+++ backend/app/api/api_v1/endpoints/blob.py
@@ -1,10 +1,14 @@
-import os
+import mimetypes
 from uuid import uuid4
 
+import magic
 from azure.storage.blob import BlobClient, ContentSettings
-from fastapi import APIRouter, File, HTTPException, UploadFile, status
+from fastapi import APIRouter, Depends, File, UploadFile, status
 from pydantic import HttpUrl
 
+from app import models
+from app.api import deps
+from app.api.api_v1.exceptions import ForbiddenFiletypeException
 from app.core.config import settings
 
 router = APIRouter()
@@ -16,30 +20,36 @@ router = APIRouter()
     response_model=HttpUrl,
     status_code=status.HTTP_200_OK,
 )
-def upload(file: UploadFile = File(...)):
+def upload(
+    file: UploadFile = File(...),
+    current_user: models.User = Depends(deps.get_current_user),
+):
     """
     Upload a video or image as a Blob to Azure Storage.
     Returns the URL of the saved file.
 
     - `file` : the file to be uploaded
     """
-    split = os.path.splitext(file.filename)
-    file_extension = split[1]
-    if file.content_type.startswith("image"):
+    # get true mimetype of file
+    mime = magic.from_buffer(file.file.read(2048), mime=True)
+    file.file.seek(0)
+
+    # pick correct storage container
+    if mime.startswith("image"):
         container = "images"
     elif file.content_type.startswith("video"):
         container = "videos"
     else:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="You can only upload an image, gif or video.",
-        )
+        raise ForbiddenFiletypeException
+
+    # upload file with a generated filename and the proper file extension
     blob = BlobClient.from_connection_string(
         conn_str=settings.AZURE_STORAGE_CREDENTIALS,
         container_name=container,
-        blob_name=uuid4().hex + file_extension,
+        blob_name=uuid4().hex + mimetypes.guess_extension(mime),
     )
-    content_settings = ContentSettings(content_type=file.content_type)
-    blob.upload_blob(file.file, content_settings=content_settings)
+    metadata = {"user_uid": current_user.uid}
+    content_settings = ContentSettings(content_type=mime)
+    blob.upload_blob(file.file, metadata=metadata, content_settings=content_settings)
 
     return blob.url
